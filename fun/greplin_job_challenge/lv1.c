4f1b373fac94adc11cf492459fc5a7ed0651be0c
Samuel <samuelololol@gmail.com>
2011-03-25 21:56:58 +0800

    rewrite lv1.c

--- fun/greplin_job_challenge/lv1.c
+++ fun/greplin_job_challenge/lv1.c
@@ -2,109 +2,82 @@
 #include <string.h>
 #include <strings.h>
 #include <stdlib.h>
+
 char blocktext[] = "FourscoreandsevenyearsagoourfaathersbroughtforthonthiscontainentanewnationconceivedinzLibertyanddedicatedtothepropositionthatallmenarecreatedequalNowweareengagedinagreahtcivilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth";
+int len;
 
-int pstr(char* str,int end, int start)
+// check reverse, node version
+int chkrvs(int pos)
 {
-    char* tmp = (char *)malloc(strlen(str)+1);
-    bzero(tmp,strlen(str)+1);
-    strncpy(tmp, str+start, end-start);
-    tmp[end-start+1]='\0';
-    printf("%s\n",tmp);
-    free(tmp);
-}
-
-// return longest postion of the same char
-int findmts(char* str){
-    if (strlen(str) <= 1)
+    if ((pos == 0)||(pos == (len -1)))
         return 0;
-    int target_p = -1;
-    int i;
-    for (i = strlen(str) -1 ; i > 0; i--){
-        if (str[i] == str[0]){
-            target_p = i;
-            break;
-        }
-    }
-    return target_p;
-}
-
-int findrvs(char* str){
-    int len_str = strlen(str);
-    if (len_str <= 1){
-        return 0;
-    }
-    int i,result;
-    char* tmpsub = (char *)malloc(len_str+1);
-    bzero(tmpsub,strlen(str)+1);
-    int mts = findmts(str);
-    if (mts == (len_str - 1)){
-        strncpy(tmpsub, str+1, mts - 1);
-        tmpsub[mts-1] = '\0';
-        result = findrvs(tmpsub);
-    }
-    else{
-        result = -1;
-    }
-    free(tmpsub);
-    return result;
-}
-
-int search_pattern(char* str, int* end)
-{
-    int len_str = strlen(str);
-    if (len_str <= 1){
-        return 0;
-    }
-    int result, check;
-    int mts = findmts(str);
-    if (mts == -1)
-        return -1;
-
-    char* tmpsub;
-    while(mts > 1)
+    int ext = 1;
+    while(blocktext[pos - ext] == blocktext[pos + ext])
     {
-        tmpsub = (char *)malloc(len_str+1);
-        bzero(tmpsub,len_str+1);
-        strncpy(tmpsub, str, mts+1);
-        tmpsub[mts+1] = '\0';
-
-        result = findrvs(tmpsub);
-
-        // not reverse-string
-        // prepare next
-        if (result == -1){
-            tmpsub[mts-2] = '\0';
-            mts = findmts(tmpsub);
-            len_str = strlen(tmpsub);
-            free(tmpsub);
-            continue;
-        }
-        else if (result == 0){
-                free(tmpsub);
-                *end = mts;
-                // nice reverse-string
-                return 0;
-        }
+        ext++;
+        if (((pos - ext) < 0) || ((pos + ext) > (len -1)))
+            break;
     }
-    return -1;
+    return ext-1;
 }
+
+// print reverse string, node version
+int ptrvs(int pos, int ct)
+{
+    int i;
+    for (i = pos - ct; i < pos+ct+1; i++) {
+        printf("%c", blocktext[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+// check reverse, slot version
+int chkrvss(int pos)
+{
+    int ft = pos;
+    int rr = pos + 1;
+    if ((pos == 0)||(pos == (len -1)))
+        return 0;
+    int ext = 0;
+    while(blocktext[ft - ext] == blocktext[rr + ext])
+    {
+        ext++;
+        if (((ft - ext) < 0) || ((rr + ext) > (len -1)))
+            break;
+    }
+    return ext;
+}
+
+// print reverse string, slot version
+int ptrvss(int pos, int ct)
+{
+    int i;
+    for (i = pos + 1 - ct; i < pos + ct + 1; i++) {
+        printf("%c", blocktext[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
 int main(int argc, const char *argv[])
 {
-    int i,mts;
-    int end=0;
+    int i,ext,sext;
     int max=0;
-    int tmp=0;
-    for (i = 0; i < strlen(blocktext) - 2; i++){
-        if(search_pattern(blocktext+i,&end) == 0){
-            if (end > max){
-                printf("%c",blocktext[i]);
-                pstr(blocktext+i+1,end,0);
-                tmp = end;
-            }
-            else
-                tmp = max;
-            max = tmp;
+    len = strlen(blocktext);
+
+    for (i = 0; i < len; i++) {
+
+        ext = chkrvs(i);
+        if ((ext != 0)&&(ext >= max)){
+            ptrvs(i,ext);
+            max = ext;
+        }
+
+        sext = chkrvss(i);
+        if ((sext != 0)&&(sext > max)){
+            ptrvss(i,sext);
+            max = ext;
         }
     }
     return 0;
